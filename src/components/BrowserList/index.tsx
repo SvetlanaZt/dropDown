3d988429a8416d92ba0bcbb10fb78823a575f4c6
SvetlanaZt <svetlana.zt23@gmail.com>
2023-03-25 12:38:03 +0200

    fin

--- src/components/BrowserList/index.tsx
+++ src/components/BrowserList/index.tsx
@@ -13,30 +13,25 @@ interface Props {
   typingName?: string;
   setTypingName: Function;
 }
-export const BrowserList = React.memo(
-  function BrowserList({ typingName, setTypingName }: Props) {
-    console.log("BrowserList");
+export function BrowserList({ typingName, setTypingName }: Props) {
+  const browsers = listBrowser?.filter((item) =>
+    item?.toLowerCase().includes((typingName ?? "").toLowerCase())
+  );
+  const onClick = (country: string) => {
+    setTypingName(country);
+  };
 
-    const browsers = listBrowser?.filter((item) =>
-      item?.toLowerCase().includes((typingName ?? "").toLowerCase())
-    );
-    const onClick = (country: string) => {
-      setTypingName(country);
-    };
-
-    return (
-      <ul className={s.listBrowsers}>
-        {browsers?.map((country) => (
-          <li
-            key={country}
-            onClick={() => onClick(country)}
-            className={s.browser}
-          >
-            {country}
-          </li>
-        ))}
-      </ul>
-    );
-  },
-  () => true
-);
+  return (
+    <ul className={s.listBrowsers}>
+      {browsers?.map((country) => (
+        <li
+          key={country}
+          onClick={() => onClick(country)}
+          className={s.browser}
+        >
+          {country}
+        </li>
+      ))}
+    </ul>
+  );
+}
